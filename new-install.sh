dc6c3b3ba0f3ba34f30c8b88eb7443756d36756e
Andrew Rady <andrew.rady@ritterim.com>
2020-09-15 12:15:01 -0600

    spelling is hard...

--- new-install.sh
+++ new-install.sh
@@ -10,7 +10,7 @@ echo "brew install git"
 echo "install ruby through rvm"
 echo "gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 echo "\curl -sSL https://get.rvm.io | bash -s stable --ruby"
-echo "gem install bunlder"
+echo "gem install bundler"
 echo "gem install jekyll"
 
 #node/npm
